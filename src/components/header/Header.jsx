35e73f72187a484e4cbc77fb8ba8dde1f1a1f9ea
Neto <wantedshark34@gmail.com>
2025-07-18 16:57:20 -0300

    header finished

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-import { FaShoppingCart } from "react-icons/fa";
-import logo from "../../assets/logo.png"
+import logo from "../../assets/logo.png";
+import pao from "../../assets/pao.jpg";
+import bolo from "../../assets/bolo.jpg";
 import "./header.css";
 
 export default function Header() {
+  const products = [
+    { img: pao, name: "Pão" },
+    { img: bolo, name: "Bolo" },
+  ];
+
   return (
     <div className="header">
       <div className="logo">
@@ -11,15 +17,30 @@ export default function Header() {
           <img className="logo_img" src={logo} alt="logo" />
         </div>
         <div className="cart">
-          <button className="cart_button">
-            <FaShoppingCart size={26} fill="#FFCC00" />
-          </button>
+          <button className="cart_button">cancelar pedido</button>
         </div>
       </div>
       <div className="search">
-        <input type="text" className="search_input" placeholder="Bolo de formigueiro" />
+        <input
+          type="text"
+          className="search_input"
+          placeholder="Bolo de formigueiro"
+        />
       </div>
-      <div className="categories">Categories of products</div>
+      <div className="categories">
+        {products.map((prod, i) => (
+          <button className="categories_button" key={i}>
+            <div className="categories_item">
+              <img
+                src={prod.img}
+                alt="categorie"
+                className="categories_item_img"
+              />
+              <p className="categories_item_name">{prod.name}</p>
+            </div>
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
